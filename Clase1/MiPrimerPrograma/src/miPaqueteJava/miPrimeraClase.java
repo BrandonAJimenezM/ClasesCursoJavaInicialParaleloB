ec659e0fab551f5dd86d8abfdb9e5d404a386502
brandon <jmbrandon15@gmail.com>
2026-03-10 01:27:32 -0500

    UploadPrimerPrograma

---
+++ Clase1/MiPrimerPrograma/src/miPaqueteJava/miPrimeraClase.java
@@ -0,0 +1,15 @@
+package miPaqueteJava;
+
+public class miPrimeraClase {
+	
+	// control + espacio -> ATAJO
+	
+	public static void main(String[] args) {
+		// alt + 92 '\'
+		System.out.println("“Mi nombre es Alejandro Taboada tengo 18 años,\n"
+				+ "estoy  estudiando Tecnologías de la Información y la Comunicación en la ESPE/UCE...\r\n"
+				+ "Me gusta la musica, juego Futbol, mi sueño es... \r\n"
+				+ "Me intereso/me gusto/me dio curiosidad la programación porque es bonita\n"
+				+ "y me agrada“");
+	}
+}
